13964990d82f1f9c29b4070540d098fada600765
Andy Smith <ajsmith@args.us>
2016-08-08 18:20:23 -0400

    Somewhat working edit page

--- client/pages/team/Edit.jsx
+++ client/pages/team/Edit.jsx
@@ -2,13 +2,59 @@ import React from "react";
 import TrackerReact from "meteor/ultimatejs:tracker-react";
 import Card from "../../components/Card.jsx";
 import AccountsUI from "../../components/AccountsUI.jsx";
+import DOMPurify from "dompurify";
 
-export default class Team extends TrackerReact(React.Component) {
+export default class Edit extends TrackerReact(React.Component) {
+
+	constructor(props) {
+		super(props);
+		Meteor.call("editor.getPage", {page: "Test"}, function(err, response) {
+			Session.set('page', response);
+		});
+
+	}
+
+	getId() {
+		return Math.floor(Math.random() * 99999999) + 10000000;
+	}
+
+	getPage(page) {
+		return Pages.findOne({"name": page});
+	}
+
+	saveChanges() {
+		Meteor.call("editor.updatePage", {
+			"page": "Test",
+			"content": $('.editor').summernote('code')
+		}, function (err, response) {
+			if (response) {
+				Bert.alert({
+					message: "Saved!",
+					type: "success",
+					style: "growl-top-right",
+					icon: "fa-check"
+				});
+			}
+		});
+	}
+
+	getPageContent() {
+		return Session.get('page');
+	}
+
+	editPage() {
+		$('.editor').summernote({
+			fontNames: ['Open Sans', 'Coo Hew'],
+			fontNamesIgnoreCheck: ['Open Sans', 'Coo Hew']
+		});
+
+		$('.editor').each(function (i, obj) {
+			$('.editor').eq(i).summernote('code', Session.get("page").cards[i].content);
+		});
+	}
 
 	render() {
-		Accounts.onLogin(function () {
-			FlowRouter.go("/team");
-		});
+		let page = this.getPageContent();
 
 		if (!Accounts.userId()) {
 			FlowRouter.go("/team/login");
@@ -16,8 +62,14 @@ export default class Team extends TrackerReact(React.Component) {
 
 		const name = Accounts.user() ? Accounts.user().profile.name : "";
 
+		if (!page) {
+			return (
+				<div></div>
+			)
+		}
+
 		return (
-			<div>
+			<div className="edit">
 				<Card title={<div><AccountsUI /></div>} content={
 					<div>
 						<ul className="nav nav-tabs">
@@ -27,9 +79,27 @@ export default class Team extends TrackerReact(React.Component) {
 							<li role="presentation"><a href="#">Users</a></li>
 							<li role="presentation"><a href="#">Settings</a></li>
 						</ul>
+						<p>
+							Choose a page to edit:
+							<select>
+								<option>Sponsors</option>
+							</select>
+							<br />
+							<button className="save-changes btn btn-primary" onClick={this.editPage}>Edit Page</button><br />
+							<button className="save-changes btn btn-success" onClick={this.saveChanges}>Save Changes</button>
+							<button className="save-changes btn btn-default" onClick={this.addCard}>Add Card</button>
+						</p>
 					</div>
 				}/>
+
+				{page.cards.map( (card)=>{
+					return <Card key={this.getId()} title={card.title} className="editor" content={
+							<div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(card.content)}}></div>
+						}/>
+				} )}
+
 			</div>
+
 		)
 	}
 }
