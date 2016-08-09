7ec86393e4dd110dcd4bebb144b44940b28cf946
Andy Smith <ajsmith@args.us>
2016-08-09 05:21:50 -0400

    Subscription for the base team/edit page so the edit list stays up-to-date.

--- server/publish.js
+++ server/publish.js
@@ -5,3 +5,11 @@ Pages = new Mongo.Collection("pages");
 Meteor.publish("getPage", function(page) {
 	return Pages.find({"name": page});
 });
+
+Meteor.publish("editor.getPages", function() {
+	if (!Roles.userIsInRole(this.userId, ['admin', 'editor'])) {
+		throw new Meteor.Error('not-authorized');
+	}
+
+	return Pages.find();
+});
