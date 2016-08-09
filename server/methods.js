2568aead59a7d7cb404623410b7e9277234f2d09
Andy Smith <ajsmith@args.us>
2016-08-09 05:20:49 -0400

    New methods for updating cards and pages. Added security to make sure logging in users have an appropriate domain on their email.

--- server/methods.js
+++ server/methods.js
@@ -1,17 +1,43 @@
+import { default as UUID } from "node-uuid";
+
+function authenticate(roles) {
+	if (!Roles.userIsInRole(Meteor.userId(), roles)) {
+		throw new Meteor.Error('not-authorized');
+	}
+}
+
 Meteor.methods({
 	'editor.getPage'({page}) {
-		if (!Roles.userIsInRole(Meteor.userId(), ['admin', 'editor'])) {
-			throw new Meteor.Error('not-authorized');
-		}
-		
+		authenticate(['admin', 'editor']);
 		return Pages.findOne({"name": page});
 	},
-	'editor.updatePage'({page, content}) {
-		if (!Roles.userIsInRole(Meteor.userId(), ['admin', 'editor'])) {
-			throw new Meteor.Error('not-authorized');
-		}
 
-		Pages.update({"name": page}, {$set: {"cards.0.content": content}});
+	'editor.updatePage'({page, cards}) {
+		authenticate(['admin', 'editor']);
+		return Pages.update({"name": page}, {$set: {"cards": cards}});
+	},
+
+	'editor.createPage'(name) {
+		authenticate(['admin', 'editor']);
+		Pages.insert({
+			"name": name,
+			"cards": []
+		});
+	},
+
+	'editor.deletePage'(name) {
+		authenticate(['admin', 'editor']);
+		Pages.remove({"name": name});
+	},
+
+	'editor.addCard'(name) {
+		authenticate(['admin', 'editor']);
+		Pages.update({"name": name}, {$push: {"cards": {"id": UUID.v4(),"title": "", "content": ""}}});
+	},
+
+	'editor.deleteCard'({name, id}) {
+		authenticate(['admin', 'editor']);
+		Pages.update({"name": name}, {$pull: {"cards": {"id": id}}});
 	}
 });
 
@@ -23,3 +49,15 @@ Meteor.users.deny({
 		return true;
 	}
 });
+
+
+/* Validate the login attempt so that only users with emails ending in certain
+ sub-domains are allowed. We don't want any random Google user logging in. */
+
+Accounts.validateLoginAttempt(function(info) {
+	return userIsValid(info.user);
+});
+
+function userIsValid(user) {
+	return user.services.google.email.endsWith("@args.us") || user.services.google.email.endsWith("@argsrobotics.com");
+}
