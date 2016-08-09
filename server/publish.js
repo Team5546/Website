a5866a13ab47c558b165605a351eaee95e60f0e4
Andy Smith <ajsmith@args.us>
2016-08-09 19:11:46 -0400

    Added checks and a getPageByName function for Page.jsx

--- server/publish.js
+++ server/publish.js
@@ -3,11 +3,17 @@ import { Meteor } from 'meteor/meteor';
 Pages = new Mongo.Collection("pages");
 
 Meteor.publish("editor.getPage", function(id) {
+	check(id, String);
 	if (!Roles.userIsInRole(this.userId, ['admin', 'editor'])) {
 		throw new Meteor.Error('not-authorized');
 	}
 
 	return Pages.find({"_id": id});
+});
+
+Meteor.publish("editor.getPageByName", function(name) {
+	check(name, String);
+	return Pages.find({"name": name});
 });
 
 Meteor.publish("editor.getPages", function() {
