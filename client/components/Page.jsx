bae05593a2aa46c470a90790337780a9df21edba
Andy Smith <ajsmith@args.us>
2016-08-09 19:14:06 -0400

    Made Page.jsx work with new page editor and id vs name changes.

--- client/components/Page.jsx
+++ client/components/Page.jsx
@@ -1,52 +1,40 @@
-import React, {Component} from "react";
+import React from "react";
 import TrackerReact from "meteor/ultimatejs:tracker-react";
 import Card from "../components/Card.jsx";
 import DOMPurify from "dompurify";
 
-Pages = new Mongo.Collection("pages");
-
-export default class Page extends TrackerReact(Component) {
+export default class Page extends TrackerReact(React.Component) {
 
 	constructor(props) {
 		super(props);
 
-		this.state = {
-			subscription: {
-				pages: Meteor.subscribe("getPage", this.props.name)
-			}
-		}
+		Meteor.subscribe("editor.getPageByName", this.props.name);
 	}
 
-	componentWillUnmount() {
-		this.state.subscription.pages.stop();
-	}
-
-	getPage(page) {
-		return Pages.findOne({"name": page});
+	getPage(name) {
+		return Pages.findOne({"name": name});
 	}
 
 	render() {
 		let page = this.getPage(this.props.name);
 
-		if (page) {
+		if (!page) {
 			return (
-				<div>
-					{page.cards.map( (card)=>{
-						return <Card key={card.title} title={card.title} content={
-							<div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(card.content)}}></div>
-						}/>
-					} )}
-
-					<p id="test">Test</p>
-
-				</div>
-			)
-		} else {
-			return (
-				<div>
-
-				</div>
+				<div></div>
 			)
 		}
+
+		document.title = page.title + titleSuffix;
+
+		return (
+			<div>
+				{page.cards.map( (card)=>{
+					return <Card key={card.title} title={card.title} content={
+						<div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(card.content)}}></div>
+					}/>
+				} )}
+			</div>
+		)
+
 	}
 }
