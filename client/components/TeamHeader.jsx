af95ba8af1fca242f5205981aa8afcf4c2dd397b
Andy Smith <ajsmith@args.us>
2016-08-15 19:01:09 -0400

    Remove admin tabs from team page if not admin.

--- client/components/TeamHeader.jsx
+++ client/components/TeamHeader.jsx
@@ -10,12 +10,21 @@ export default class TeamHeader extends TrackerReact(React.Component) {
 		let editor = active == "editor" ? "active" : "";
 		let users = active == "users" ? "active" : "";
 
+		if (Roles.getRolesForUser(Meteor.userId()).indexOf("admin") != -1) {
+			return (
+				<ul className="nav nav-tabs">
+					<li role="presentation" className={home}><a href="/team">Home</a></li>
+					<li role="presentation" className={editor}><a href="/team/edit">Page Editor</a></li>
+					<li role="presentation" className="disabled"><a href="#">Alerts</a></li>
+					<li role="presentation" className={users}><a href="/team/users">Users</a></li>
+					<li role="presentation" className="disabled"><a href="#">Settings</a></li>
+				</ul>
+			)
+		}
+
 		return (
 			<ul className="nav nav-tabs">
 				<li role="presentation" className={home}><a href="/team">Home</a></li>
-				<li role="presentation" className={editor}><a href="/team/edit">Page Editor</a></li>
-				<li role="presentation" className="disabled"><a href="#">Alerts</a></li>
-				<li role="presentation" className={users}><a href="/team/users">Users</a></li>
 				<li role="presentation" className="disabled"><a href="#">Settings</a></li>
 			</ul>
 		)
