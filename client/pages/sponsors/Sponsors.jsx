44fb69d7afc3d673d1cc59249bf68d102dc6c30f
Andy Smith <ajsmith@args.us>
2016-12-08 21:35:24 -0500

    Show sponsor name if no image

--- client/pages/sponsors/Sponsors.jsx
+++ client/pages/sponsors/Sponsors.jsx
@@ -35,13 +35,23 @@ export default class Sponsors extends TrackerReact(React.Component) {
 							return;
 						}
 
-						return (
-							<Card key={sponsor._id} content={
+						if (sponsor.image) {
+							return (
+								<Card key={sponsor._id} content={
 								<div>
 									<a href={sponsor.website}><img id={sponsor._id} alt={sponsor.name} src={"https://static.argsrobotics.com/" + sponsor.image} className="sponsor img-responsive center-block" /></a>
 								</div>
 							}/>
-						)
+							)
+						} else {
+							return (
+								<Card key={sponsor._id} content={
+								<div>
+									<a href={sponsor.website}>{sponsor.name}</a>
+								</div>
+							}/>
+							)
+						}
 					})}
 				</div>
 
